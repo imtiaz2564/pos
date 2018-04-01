acc82219793f003d6bb1ddeff3b43c22a01b1162
imtiaz2564 <imtiazaiub1@gmail.com>
2018-04-01 23:14:59 +0600

    updated ab stock last

--- application/views/items/journal_form.php
+++ application/views/items/journal_form.php
@@ -21,32 +21,6 @@
                     }?>
                 </div>
         <?=$form_close?>
-        <?$uri = $this->uri->segment(2); if( $this->uri->segment(2) == 'out' ) { ?>   
-        <!-- <div class="form-group">
-            <label>Customer ID :</label>
-            <input type="text" name="idCustomer" value="" class="form-control" placeholder="Customer ID" />
-        </div> -->
-        <!-- <div class="form-group">
-        <label>Business Name ( Customer ) :</label>
-                <select name = "idCustomer" class="form-control autocomplete">
-                <? //foreach($people as $people) {?>
-                <option  value = <?//=$people['id']?>><?//=$people['businessName']?></option>
-                <?// } ?>
-            </select>
-            </div>    -->
-            <div class="col-md-3">                        
-                <div class="form-group">
-                    <label>Customer Name :</label>
-                    <input type="text" name="customer" value="" class="form-control" placeholder="Customer Name">
-                </div>
-            </div>
-            <div class="col-md-3">                        
-                <div class="form-group">
-                    <label>Phone :</label>
-                    <input type="text" name="phone" value="" class="form-control" placeholder="Phone">
-                </div>
-            </div>
-            <? } ?>
             <?//$uri = $this->uri->segment(2); if( $this->uri->segment(2) == 'in' ) { ?>   
             <!-- <div class="col-md-6">                        
                 <div class="form-group">
@@ -68,44 +42,48 @@
     <? //$uri = $this->uri->segment(2); if( $this->uri->segment(2) == 'out' ) { ?>
     <div class="row">
         <div  class= "input-group customerInfo" id = "cusinfo" style="">
-        <div class= "input-group customerInfo"><h1>Customer Info</h1></div>
-        <div class= "input-group customerInfo">
-            <label>Customer Name: </label>
-            <label style="color:#0000FF" id="cusName"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>Business Name: </label>
-            <label style="color:#0000FF" id="businessName"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>Customer Email: </label>
-            <label style="color:#0000FF" id="email"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>Address: </label>
-            <label style="color:#0000FF" id="address"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>Business Address: </label>
-            <label style="color:#0000FF" id="businessAddress"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>Area: </label>
-            <label style="color:#0000FF" id="area"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>District: </label>
-            <label style="color:#0000FF" id="district"></label>
-        </div>
-        <div class= "input-group customerInfo">
-            <label>Opening Balance: </label>
-            <label style="color:#0000FF" id="openingBalance"></label>
-        <div class= "input-group customerInfo">
-            <label>Current Balance: </label>
-            <label style="color:#0000FF" id="currentBalance"></label>
+            <div class= "input-group customerInfo"><h1>Customer Info</h1></div>
+                <div class= "input-group customerInfo">
+                    <label>Customer ID: </label>
+                    <label style="color:#0000FF" id="cusCode"></label>
+                </div>
+                <div class= "input-group customerInfo">
+                    <label>Customer Name: </label>
+                    <label style="color:#0000FF" id="cusName"></label>
+                </div>
+                <div class= "input-group customerInfo">
+                    <label>Business Name: </label>
+                    <label style="color:#0000FF" id="businessName"></label>
+                </div>
+                <!-- <div class= "input-group customerInfo">
+                    <label>Customer Email: </label>
+                    <label style="color:#0000FF" id="email"></label>
+                </div>
+                <div class= "input-group customerInfo">
+                    <label>Address: </label>
+                    <label style="color:#0000FF" id="address"></label>
+                </div>
+                <div class= "input-group customerInfo">
+                    <label>Business Address: </label>
+                    <label style="color:#0000FF" id="businessAddress"></label>
+                </div> -->
+                <div class= "input-group customerInfo">
+                    <label>Area: </label>
+                    <label style="color:#0000FF" id="area"></label>
+                </div>
+                <!-- <div class= "input-group customerInfo">
+                    <label>District: </label>
+                    <label style="color:#0000FF" id="district"></label>
+                </div>
+                <div class= "input-group customerInfo">
+                    <label>Opening Balance: </label>
+                    <label style="color:#0000FF" id="openingBalance"></label>
+                <div class= "input-group customerInfo">
+                    <label>Current Balance: </label>
+                    <label style="color:#0000FF" id="currentBalance"></label>
+                </div> -->
         </div>
     </div>
-</div>
 <? //} ?>
 <?// $uri = $this->uri->segment(2); if( $this->uri->segment(2) == 'in' ) { ?>
     <!-- <div><h1>Supplier Info</h1></div>
@@ -160,7 +138,7 @@
     <div  class="form-group delivery" id = "deliveryType" style='float: right;'>
         <label class="form-group delivery">Delivery Type: </label> 
         <select class="form-group delivery"  name = "deliveryType" >
-            <option class="form-group delivery" value="0">None</option>
+            <option class="form-group delivery" value=" ">None</option>
             <option class="form-group delivery" value="truck">Truck</option>
             <option class="form-group delivery" value="thela">Thela</option>
         </select>
@@ -196,7 +174,7 @@
         $('#deliveryType').hide();
     });
 var journalId = '<?=$this->uri->segment(4)?>';
-var delivery = $('select[name=deliveryType]').val();
+// var delivery = $('select[name=deliveryType]').val();
 $('select[name=deliveryType]').on('change', function(){
 
     $.ajax({
@@ -294,19 +272,20 @@ $.ajaxSetup({ cache: false });
             openingBalance = data["openingBalance"];
             currentBalance = data["totalBalance"]; 
             //$('input[name=idCustomer]').val(customer_code);
-            $('input[name=phone]').val(phone);
-            $('input[name=customer]').val(name);
-        
+            // $('input[name=phone]').val(phone);
+            // $('input[name=customer]').val(name);
+            
+            $('#cusCode').html(customer_code);
             $('#cusId').val(customerID);
             $('#cusName').html(name);
             $('#businessName').html(businessName);
-            $('#email').html(email);
-            $('#address').html(address);
-            $('#businessAddress').html(businessAddress);
+           // $('#email').html(email);
+            // $('#address').html(address);
+            // $('#businessAddress').html(businessAddress);
             $('#area').html(area);
-            $('#district').html(district);
-            $('#openingBalance').html(openingBalance);
-            $('#currentBalance').html(currentBalance);
+            // $('#district').html(district);
+            // $('#openingBalance').html(openingBalance);
+            // $('#currentBalance').html(currentBalance);
         }
     });
         
@@ -330,11 +309,11 @@ $.ajaxSetup({ cache: false });
 <?}?>
 $('#formJournal').submit(function() {
     <? $uri = $this->uri->segment(2); if( $this->uri->segment(2) == 'out' ) { ?>
-
-    if( delivery == 0 ){
-        alert('Select delivery type');
-        return false;
-    }
+        var delivery = $('select[name=deliveryType]').val();
+        if( delivery == " " ){
+            alert('Select delivery type');
+            return false;
+        }
 <? } ?>
     cus_ID = parseInt($('#cusId').val());
     sup_ID = parseInt($('#supId').val());
