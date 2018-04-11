479142e6961f62e86e2639ecdd2842dc510c955d
imtiaz2564 <imtiazaiub1@gmail.com>
2018-04-11 16:36:39 +0600

    added user model again

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,5 +1,6 @@
-<?php
-class User_Model extends CI_Model{
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User_model extends CI_Model{
     function __construct(){
         parent::__construct();
     }
